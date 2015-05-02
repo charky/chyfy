2e3c49fc3d5dfdc608811487aeb867511c93084b
charky <charky@uPepper>
2015-05-02 23:36:50 +0200

    Buildscript angepasst. Nun entsteht auch ein installierbares Zip. Ortner musste mit includiert sein. Script ist lauffähig.

--- chyfy.py
+++ chyfy.py
@@ -17,33 +17,17 @@ You should have received a copy of the GNU General Public License
 along with ChyFy.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-import xbmcaddon
-import xbmcgui
 import sys
-
-#Set global addon information first
-__addon_id__ = 'script.audio.chyfy'
-addon_cfg = xbmcaddon.Addon(__addon_id__)
-__addon_path__ = addon_cfg.getAddonInfo('path')
-__addon_version__ = addon_cfg.getAddonInfo('version')
-DEFAULT_FOLDER_IMG = 'DefaultFolder.png'
-
+import xbmcgui
+import xbmcplugin
 
 addon_handle = int(sys.argv[1])
-url_path = sys.argv[0] + '?' + 'Inbox'
 
-#Hello Wolrd 
-addonname   = addon_cfg.getAddonInfo('name')
- 
-line1 = "Hello World!"
-line2 = "We can write anything we want here"
-line3 = "Using Python"
- 
-xbmcgui.Dialog().ok(addonname, line1, line2, line3) 
+xbmcplugin.setContent(addon_handle, 'audios')
 
-item = xbmcgui.ListItem("FolderItem1", iconImage = DEFAULT_FOLDER_IMG)
-xbmcplugin.addDirectoryItem(handle = addon_handle, 
-                                    url = url_path, listitem = item, 
-                                    isFolder = True) 
+url = 'http://localhost/some_video.mkv'
+li = xbmcgui.ListItem('My First Video!', iconImage='DefaultAudio.png')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 
+xbmcplugin.endOfDirectory(addon_handle)
  
